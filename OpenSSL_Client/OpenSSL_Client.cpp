ac2c243f786bbd8b68deea367d05ea8571f5a3a0
Eugenio Miró <eugenio.miro@gmail.com>
2022-10-11 18:26:10 -0300

    add openssl library and first code

--- OpenSSL_Client/OpenSSL_Client.cpp
+++ OpenSSL_Client/OpenSSL_Client.cpp
@@ -2,11 +2,40 @@
 //
 
 #include "OpenSSL_Client.h"
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+#include <memory>
 
 using namespace std;
 
+namespace my {
+
+	template<class T> struct DeleterOf;
+	template<> struct DeleterOf<SSL_CTX> { void operator()(SSL_CTX* p) const { SSL_CTX_free(p); } };
+	template<> struct DeleterOf<SSL> { void operator()(SSL* p) const { SSL_free(p); } };
+	template<> struct DeleterOf<BIO> { void operator()(BIO* p) const { BIO_free_all(p); } };
+	template<> struct DeleterOf<BIO_METHOD> { void operator()(BIO_METHOD* p) const { BIO_meth_free(p); } };
+
+	template<class OpenSSLType>
+	using UniquePtr = std::unique_ptr<OpenSSLType, my::DeleterOf<OpenSSLType>>;
+
+	[[noreturn]] void print_errors_and_exit(const char* message)
+	{
+		fprintf(stderr, "%s\n", message);
+		ERR_print_errors_fp(stderr);
+		exit(1);
+	}
+
+} // namespace my
+
+
 int main()
 {
-	cout << "Hello CMake." << endl;
+	auto ctx = my::UniquePtr<SSL_CTX>(SSL_CTX_new(TLS_client_method()));
+
+	auto bio = my::UniquePtr<BIO>(BIO_new_connect("example.com:80"));
+	if (BIO_do_connect(bio.get()) <= 0) {
+		my::print_errors_and_exit("Error in BIO_do_connect");
+	}
 	return 0;
 }

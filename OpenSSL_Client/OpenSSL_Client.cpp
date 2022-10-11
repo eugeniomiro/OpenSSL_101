fde70ff2b725e5b5847845deaea9ff5d98270fe0
Eugenio Miró <em@mercurydigitalassets.com>
2022-10-11 16:53:23 -0300

    first commit

---
+++ OpenSSL_Client/OpenSSL_Client.cpp
@@ -0,0 +1,12 @@
+﻿// OpenSSL_Client.cpp : Defines the entry point for the application.
+//
+
+#include "OpenSSL_Client.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
